7c1cfd8c6a515cd9c5f275f7fd278e41a662eeb0
Blake <tk0wo3@gmail.com>
2023-02-16 13:15:00 +0800

    2469. Convert the Temperature

---
+++ 2469-Convert-the-Temperature.php
@@ -0,0 +1,11 @@
+<?php
+class Solution {
+
+    /**
+     * @param Float $celsius
+     * @return Float[]
+     */
+    function convertTemperature($celsius) {
+        return [$celsius + 273.15, ($celsius * 1.80) + 32];
+    }
+}
